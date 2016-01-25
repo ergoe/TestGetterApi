5c7553d7e9e28c177f19c2c37eaac1f8e10f7f3b
Eric Goeckeritz <Eric@bbk006515.body.local>
2016-01-25 14:06:53 -0700

    update to testCase log sql to order in desc order to fail reason is first

--- sql/getTestCaseLog.sql
+++ sql/getTestCaseLog.sql
@@ -1,4 +1,11 @@
-SELECT      * 
-FROM        TestCaseLog
-WHERE
-            TestCaseExecution_id = @testId 
+SELECT  Id as logId
+        , EntryTime AS entryTime
+        , Level AS logLevel
+        , LogMessage AS logMessage
+        , StackTrace AS stackTrace
+        , ScreenShotLink AS screenShotLink
+        , HtmlSourceLink AS htmlSourceLink
+        , TestCaseExecution_id as testCaseExecutionId
+FROM TestCaseLog
+WHERE     TestCaseExecution_id = @testId
+ORDER BY logId DESC 
